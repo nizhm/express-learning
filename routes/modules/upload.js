09bddbe4df58b2487a7bdee227c7fb402cc0126f
Zhiming Ni <1078830088@qq.com>
2021-08-14 23:08:11 +0800

    learn delete image

--- routes/modules/upload.js
+++ routes/modules/upload.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 const formidable = require('formidable');
+const log = require('../../utils/log');
 
 /* GET users listing. */
 router.use((req, res, next) => {
@@ -31,30 +32,70 @@ router.post('/image', (req, res) => {
     const file = files[''];
     const oldPath = file.path;
     const extension = oldPath.slice(oldPath.lastIndexOf('.'));
-    const newPath = path.join(options.uploadDir, `upload_${ Date.now() + extension }`);
+    const imgName = `upload_${ Date.now() + extension }`;
+    const newPath = path.join(options.uploadDir, imgName);
     fs.rename(oldPath, newPath, err => {
       if(err) {
-        console.error(err)
+        log.error(err);
       }else {
-        console.log('Rename successfully')
+        log.info('Rename successfully');
+        resJSON.data = { name: imgName };
       }
-    })
-    console.log(file)
-  })
+    });
+  });
   setTimeout(() => {
     res.status(200).json(resJSON)
   }, 1000)
-})
+});
+
 // 删除图片
-router.delete('/deleteImage', (req, res) => {
+router.delete('/deleteImg', (req, res) => {
   const resJSON = {
     code: 200,
     data: null,
     msg: 'delete success'
+  };
+  const imgName = req.body.name;
+  if(!imgName) {
+    resJSON.code = 201;
+    resJSON.msg = 'Image name is required';
+  }else {
+    const imgPath = path.join('common', 'file', 'images', imgName);
+    fs.rm(imgPath, err => {
+      if(err) {
+        throw new Error(err);
+      }else {
+        log.info(`Successfully delete "${ imgName }"`);
+      }
+    })
   }
   setTimeout(() => {
-    res.status(200).json(resJSON)
+    res.status(200).json(resJSON);
   }, 1000)
+});
+
+// 获取图片
+router.post('/fetchImg', (req, res) => {
+  const imgName = req.body.name;
+  if(!imgName) {
+    const resJSON = {
+      code: 201,
+      data: null,
+      msg: 'Image name is required'
+    };
+    res.status(200).json(resJSON);
+  }else {
+    const options = {
+      root: path.join('common', 'file', 'images'),
+    }
+    res.sendFile(imgName, options, err => {
+      if (err) {
+        throw err;
+      } else {
+        log.info(`Successfully send image ${ imgName }`)
+      }
+    })
+  }
 })
 
 module.exports = router;
